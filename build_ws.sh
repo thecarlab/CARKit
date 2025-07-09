9ba1161ec289544a807d30b769cc96f893f049f5
Yuxin Wang <87698138+Ariiees@users.noreply.github.com>
2025-07-09 16:46:28 -0400

    Update build_ws.sh
    Signed-off-by: Yuxin Wang <87698138+Ariiees@users.noreply.github.com>

--- build_ws.sh
+++ build_ws.sh
@@ -6,7 +6,7 @@ echo "======================================="
 echo "Build NavOS workspace"
 echo "======================================="
 
-echo "[1/2] Building workspace with colcon..."
+echo "[1/2] Building workspace with colcon (15 mins expected)..."
 cd ~/NavOS
 colcon build --symlink-install --executor sequential
 
@@ -14,9 +14,9 @@ SOURCE_LINE="source ~/NavOS/install/setup.bash"
 if ! grep -Fxq "$SOURCE_LINE" ~/.bashrc; then
     echo "[2/2] Adding source line to ~/.bashrc..."
     echo "$SOURCE_LINE" >> ~/.bashrc
-    echo "✅ Added to ~/.bashrc: $SOURCE_LINE"
+    echo "✅ NavOS path added to ~/.bashrc: $SOURCE_LINE"
 else
-    echo "✅ ~/.bashrc already contains the source line."
+    echo "✅ ~/.bashrc already contains the NavOS path."
 fi
 
 echo "======================================="
@@ -28,27 +28,30 @@ echo "======================================="
 echo "Setting up sensor drivers workspace"
 echo "======================================="
 
+echo ""
 echo "[1/5] Creating workspace at ~/sensors_ws/src..."
 mkdir -p ~/sensors_ws/src
 cd ~/sensors_ws/src
 
-
+echo ""
 echo "[2/5] Cloning RealSense ROS2 driver..."
 git clone https://github.com/IntelRealSense/realsense-ros.git
 
+echo ""
 echo "[3/5] Cloning Slamtec SLLiDAR ROS2 driver..."
 git clone https://github.com/Slamtec/sllidar_ros2.git
 
-echo "[4/5] Building workspace with colcon..."
+echo ""
+echo "[4/5] Building workspace with colcon (10 mins expected)..."
 cd ~/sensors_ws
 colcon build --symlink-install --executor sequential
 
-
+echo ""
 SOURCE_LINE="source ~/sensors_ws/install/setup.bash"
 if ! grep -Fxq "$SOURCE_LINE" ~/.bashrc; then
     echo "[5/5] Adding source line to ~/.bashrc..."
     echo "$SOURCE_LINE" >> ~/.bashrc
-    echo "✅ Added to ~/.bashrc: $SOURCE_LINE"
+    echo "✅ Sensor path added to ~/.bashrc: $SOURCE_LINE"
 else
     echo "✅ ~/.bashrc already contains the source line."
 fi
@@ -57,7 +60,19 @@ echo "======================================="
 echo "✅ Sensor Driver Installation complete!"
 echo "======================================="
 
-echo "[FINAL] Sourcing the workspace in this shell..."
+echo ""
+echo "[FINAL 1/2] Sourcing the workspace in this shell..."
 source ~/NavOS/install/setup.bash
 source ~/sensors_ws/install/setup.bash
+
+echo ""
+echo "[FINAL 2/2] Creating udev rules for rplidar..."
+sudo chmod 777 /dev/ttyUSB0
+
+
+echo "======================================="
+echo "✅ ALL SET!"
+echo "Check the README.md for Tutorial!"
+echo "======================================="
+
 
