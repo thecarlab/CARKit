b55174914ce64e4913c637875f255f894488c990
Yuxin Wang <87698138+Ariiees@users.noreply.github.com>
2026-05-18 12:13:38 -0400

    Fix ROS setup sourcing in Docker build script

--- docker/build_workspace.sh
+++ docker/build_workspace.sh
@@ -8,13 +8,17 @@ cd "$WORKSPACE"
 
 ./carkit/setup_vendor_repos.sh
 
+set +u
 source /opt/ros/${ROS_DISTRO:-humble}/setup.bash
+set -u
 
 rosdep update
 rosdep install --from-paths carkit --ignore-src -r -y
 
 colcon build --symlink-install --executor sequential
 
+set +u
 source install/setup.bash
+set -u
 
 ros2 pkg list | grep carkit
